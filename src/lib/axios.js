b976add01a9046b66a0b8af8003c8b63c7702729
Sarthak Panigrahi <sipunp70@gmail.com>
2025-04-10 23:43:04 +0530

    axios update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chatty-app-backend-5cil.onrender.com/api",
-  withCredentials: true, // ✅ Important for cookie-based auth
+  baseURL: "https://chatty-app-backend-5cil.onrender.com/api", // your backend URL
+  withCredentials: true,
 });
