251fbd6e2e5a69fdfdf84f681b70b4f4d93b4475
Sarthak Panigrahi <sipunp70@gmail.com>
2025-04-12 13:01:42 +0530

    Fix typo in welcome message on NoChatSelected component

--- src/components/NoChatSelected.jsx
+++ src/components/NoChatSelected.jsx
@@ -19,7 +19,7 @@ const NoChatSelected = () => {
         {/* Welcome Text */}
         <h2 className="text-2xl font-bold">Welcome to MonoMate!</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
+          Select a conversation from the sidebar to start chatting ans fun
         </p>
       </div>
     </div>
